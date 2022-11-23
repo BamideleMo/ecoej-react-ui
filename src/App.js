f249b490b559d8b8db8df784adf3e2f7aaac107d
BamideleMo <bamidelemo@gmail.com>
2022-11-23 03:48:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center mt-32 text-lg w-11/12 lg:w-1/2 rounded-lg shadow bg-blue-100 p-6 lg:p-20 mx-auto">
+      <img src="/ECOEJ-logo.png" alt="ECWA College of Education, Jos" className="mx-auto w-24" />
+      <h1 className="text-3xl text-blue-900 mt-4">Welcome.</h1>
+      <p><b>This website is under development</b>. You may 
+        access the Portal for semester registration: 
+         <a href="https://portal.ecoej.org" className='text-red-600 hover:opacity-30'> portal.ecoej.org</a></p>
     </div>
   );
 }
